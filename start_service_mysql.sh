127759e8e2090781df3761723f1e31495967ab49
Nguyễn Hà Phan <nguyen.ha.phan@framgia.com>
2017-09-21 18:44:54 +0700

    Update start_service_mysql.sh

--- start_service_mysql.sh
+++ start_service_mysql.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 chmod 777 /venv/*
 sed -i -e 's/sqlalchemy.url.*/sqlalchemy.url = mysql:\/\/root:root@localhost\/framgiatw/g' alembic.ini
-dbconfig="SQLALCHEMY_DATABASE_URI = 'mysql://root:root@localhost/framgiatw'"
-sed -i -e 's/SQLALCHEMY_DATABASE_URI.*/'$dbconfig'/g' config.py
+sed -i -e 's/admin/root/g' config.py
 
 service mysql start
 mysql -uroot -proot -e "CREATE DATABASE "framgiatw""
-sleep 7
+alembic upgrade head
 python seed.py
-python run.py
 exec $@
